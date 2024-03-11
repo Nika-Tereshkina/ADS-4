93a4c1ec7b9ae8da4dd71c2bee588df75f28e784
Nika-Tereshkina <159996107+Nika-Tereshkina@users.noreply.github.com>
2024-03-11 23:36:42 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,49 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+int countPairs1(int *arr, int len, int value)
+{
+    int counter = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                counter++;
+            }
+            else if (arr[i] + arr[j] > value) {
+                break;
+            }
+        }
+    }
+    return counter;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+int countPairs2(int *arr, int len, int value)
+{
+    int counter = 0;
+    int left = 0;
+    int right = len - 1;
+    while (left < right) {
+        int summa = arr[left] + arr[right];
+        if (summa == value) {
+            counter++;
+            left++;
+            right--;
+        }
+        else if (summa < value) {
+            left++;
+        }
+        else {
+            right--;
+        }
+    }
+    return counter;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+int countPairs3(int *arr, int len, int value)
+{
+    int countук = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                counter++;
+            }
+        }
+    }    
+    return counter;
 }
