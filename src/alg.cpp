756b41f6c40ebc06cd7fe5d43027c4d8b5d25799
Nika-Tereshkina <159996107+Nika-Tereshkina@users.noreply.github.com>
2024-03-12 00:20:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,8 +5,6 @@ int countPairs1(int *arr, int len, int value) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
                 counter++;
-            } else if (arr[i] + arr[j] > value) {
-                break;
             }
         }
     }
@@ -14,30 +12,47 @@ int countPairs1(int *arr, int len, int value) {
 }
 int countPairs2(int *arr, int len, int value) {
     int counter = 0;
-    int left = 0;
-    int right = len - 1;
-    while (left < right) {
-        int summa = arr[left] + arr[right];
+    int start = 0;
+    int end = len - 1;
+    while (start < end) {
+        int summa = arr[start] + arr[end];
         if (summa == value) {
             counter++;
-            left++;
-            right--;
-        } else if (summa < value) {
-            left++;
-        } else {
-            right--;
+            int i = start + 1;
+            while (i < end && arr[i] == arr[i - 1]) {
+                i++;
+            }
+            start = i;
+            end--;
+            } else if (summa < value) {
+                start++;
+            } else {
+            end--;
         }
     }
     return counter;
 }
 int countPairs3(int *arr, int len, int value) {
     int counter = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if (arr[i] + arr[j] == value) {
+    for (int i = 0; i < len - 1; i++) {
+        int left = i + 1;
+        int right = len - 1;
+        while (left <= right) {
+            int midle = left + (right - left) / 2;
+            if (arr[i] + arr[midle] == value) {
                 counter++;
+                int j = midle + 1;
+                while (j <= right && arr[midle] == arr[j]) {
+                    counter++;
+                    j++;
+                }
+                break;
+                } else if (arr[i] + arr[midle] < value) {
+                    left = midle + 1;
+                } else {
+                    right = midle - 1;
             }
         }
-    }    
+    }
     return counter;
 }
